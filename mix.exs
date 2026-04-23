f34845d94329433f9ed1f7d1b3b2fafe4f4c2168
Mark Markaryan <markmark206@gmail.com>
2026-04-23 12:27:15 -0700

    picking up journey 0.10.54

--- mix.exs
+++ mix.exs
@@ -21,9 +21,7 @@ defmodule Introspect.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:journey, "~> 0.10.40"},
+      {:journey, "~> 0.10.53"},
       {:ecto, "~> 3.12 or ~> 3.13"},
       {:postgrex, "~> 0.20 or ~> 0.21"}
     ]
